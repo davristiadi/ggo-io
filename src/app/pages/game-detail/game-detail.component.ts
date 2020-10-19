e5e1d8063fc6ca9a14cca1027dfcf6bf53470841
dkk94 <davidkurniakristiadi@gmail.com>
2020-10-19 23:58:39 +0700

    add game-detail folder

---
+++ src/app/pages/game-detail/game-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-detail',
+  templateUrl: 'game-detaiil.component.html',
+})
+export class GameDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
